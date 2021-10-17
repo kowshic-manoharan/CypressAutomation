317552c2e041544eb0a542efea23a6ece6d90112
kowshic-manoharan <smartkowshic@gmail.com>
2021-10-17 16:29:40 +0530

    component code

---
+++ cypress/integration/3-testcases/Component.js
@@ -0,0 +1,8 @@
+describe('Component Testing', function () {
+    it('CheckBox', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('#checkBoxOption2').should('be.enabled').check().should('have.value','option2')
+        cy.get('#checkBoxOption2').should('be.enabled').uncheck().should('have.value','option2')
+        cy.get('input[type="checkbox"]').click({ multiple: true })
+    })
+})
